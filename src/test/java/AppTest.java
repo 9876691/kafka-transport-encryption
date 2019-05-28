62c5a100de2fad3e01d25c59e8c508ed508115b1
Ian Purton <ian.purton@airbus.com>
2019-05-28 07:04:33 +0000

    Initial commit

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AppTest
+{
+    public AppTest() {
+    }
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testMore()
+    {
+        assertTrue( true );
+    }
+}
