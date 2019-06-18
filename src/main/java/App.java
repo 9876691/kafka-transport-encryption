9d8ac40ed15363ca8e353976904350454d2d8d49
Ian Purton <ian.purton@gmail.com>
2019-06-18 05:53:48 +0000

    Kafka encryption PoC

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,76 +1,201 @@
-import java.security.MessageDigest;
-import java.security.SecureRandom;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-public class App 
-{
+import com.microsoft.aad.adal4j.AuthenticationContext;
+import com.microsoft.aad.adal4j.AuthenticationResult;
+import com.microsoft.aad.adal4j.ClientCredential;
+import com.microsoft.azure.keyvault.KeyVaultClient;
+import com.microsoft.azure.keyvault.authentication.KeyVaultCredentials;
+import com.microsoft.azure.keyvault.models.KeyOperationResult;
+import com.microsoft.azure.keyvault.webkey.JsonWebKeyEncryptionAlgorithm;
+import com.microsoft.rest.credentials.ServiceClientCredentials;
+
+import org.apache.kafka.clients.consumer.CommitFailedException;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class App {
     public static void main(String[] args) throws Exception {
-        String key = "abcdefghijklmop";
-        String clean = "Quisque eget odio ac lectus vestibulum faucibus eget.";
+
+        // Extract this out in .env
+        String sharedAccessKey = args[0];
+        String topic = args[1];
+        String clientId = args[2];
+        String clientSecret = args[3];
+        String eventHub = args[4];
+
+        Properties kafkaConfig = getKafkaConfig(sharedAccessKey, eventHub);
+
+        processStream(kafkaConfig, topic, clientId, clientSecret);
+    }
+
+    private static void processStream(Properties kafkaConfig, String topic,
+        String clientId, String clientSecret) throws Exception {
+
+        KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<String, byte[]>(kafkaConfig);
+
+        consumer.subscribe(Collections.singletonList(topic));
+
+        System.out.println("Polling");
+
+        String keyVaultKeyUri = null;
+        byte[] secret = null;
+        byte[] iv = null;
+
+        try {
+            while (true) {
+                final ConsumerRecords<String, byte[]> consumerRecords = consumer.poll(1000);
+                for (ConsumerRecord<String, byte[]> cr : consumerRecords) {
+                    System.out.printf("Consumer Record:(%d, %d, %d, %d)\n", cr.key(), cr.value().length, cr.partition(),
+                            cr.offset());
+
+                    byte[] siv = cr.headers().lastHeader("symmetricKeyIV").value();
+                    //siv = Arrays.copyOfRange(siv, 4, siv.length);
+                    String symmetricKeyIV = new String(siv, StandardCharsets.UTF_8);
+                    
+                    byte[] sik = cr.headers().lastHeader("symmetricKey").value();
+                    //sik = Arrays.copyOfRange(sik, 2, sik.length);
+                    String symmetricKey = new String(sik, StandardCharsets.UTF_8);
+                    
+                    String keyVersion = new String(cr.headers().lastHeader("keyVersion").value(), StandardCharsets.UTF_8).substring(2);
+                    String keyVaultKeyUriNew = new String(cr.headers().lastHeader("keyVaultKeyUri").value(), StandardCharsets.UTF_8).substring(2);
+                    byte[] encryptedPayload = cr.value();
+
+                    System.out.println(symmetricKeyIV);
+                    System.out.println(symmetricKey);
+                    System.out.println(keyVersion);
+                    System.out.println(keyVaultKeyUriNew);
+
+                    if(secret == null || ! keyVaultKeyUriNew.equals(keyVaultKeyUri)) {
+                        // We've got a new key
+                        secret = decryptKey(keyVaultKeyUriNew, symmetricKey, clientId, clientSecret);
+                        iv = decryptKey(keyVaultKeyUriNew, symmetricKeyIV, clientId, clientSecret);
+                        keyVaultKeyUri = keyVaultKeyUriNew;
+                        System.out.println("IV Length " + iv.length);
+                    }
+
+                    SecretKeySpec secretKeySpec = new SecretKeySpec(secret, "AES");
+                    IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+                    
+                    Cipher cipherDecrypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                    cipherDecrypt.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+                    byte[] decrypted = cipherDecrypt.doFinal(encryptedPayload);
+
+                    String decryptedText = new String(decrypted, StandardCharsets.UTF_8);
+
+                    System.out.println(decryptedText);
+                    //System.exit(0);
+                }
+
+                consumer.commitAsync();
+            }
+        } catch (CommitFailedException e) {
+            System.out.println("CommitFailedException: " + e);
+        } finally {
+            consumer.close();
+        }
+    }
+
+    public static byte[] decryptKey(String keyIdentifier, String textToDecrypt,
+        String clientId, String clientSecret) throws Exception {
+
+        ServiceClientCredentials credentials = createCredentials(clientId, clientSecret);
         
-        byte[] encrypted = encrypt(clean, key);
-        String decrypted = decrypt(encrypted, key);
+        KeyVaultClient keyVaultClient = new KeyVaultClient(credentials);
+
+        byte[] byteText = Base64.getDecoder().decode(textToDecrypt);
+
+        KeyOperationResult result = keyVaultClient.decrypt(keyIdentifier, JsonWebKeyEncryptionAlgorithm.RSA_OAEP, byteText);
+
+        return result.result();
     }
 
-    public static byte[] encrypt(String plainText, String key) throws Exception {
-        byte[] clean = plainText.getBytes();
+    public static AuthenticationResult getAccessToken(String authorization, String resource, String clientId, String clientSecret) 
+        throws Exception {
+        
+		if (clientId == null) {
+			throw new Exception("Please inform arm.clientid in the environment settings.");
+		}
 
-        // Generating IV.
-        int ivSize = 16;
-        byte[] iv = new byte[ivSize];
-        SecureRandom random = new SecureRandom();
-        random.nextBytes(iv);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+		AuthenticationResult result = null;
+		ExecutorService service = null;
+		try {
+			service = Executors.newFixedThreadPool(1);
+			AuthenticationContext context = new AuthenticationContext(authorization, false, service);
 
-        // Hashing key.
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        digest.update(key.getBytes("UTF-8"));
-        byte[] keyBytes = new byte[16];
-        System.arraycopy(digest.digest(), 0, keyBytes, 0, keyBytes.length);
-        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+			ClientCredential credentials = new ClientCredential(clientId, clientSecret);
+			Future<AuthenticationResult> future = context.acquireToken(resource, credentials, null);
 
-        // Encrypt.
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
-        byte[] encrypted = cipher.doFinal(clean);
+			if (future == null) {
+				throw new Exception("Missing or ambiguous credentials.");
+			}
 
-        // Combine IV and encrypted part.
-        byte[] encryptedIVAndText = new byte[ivSize + encrypted.length];
-        System.arraycopy(iv, 0, encryptedIVAndText, 0, ivSize);
-        System.arraycopy(encrypted, 0, encryptedIVAndText, ivSize, encrypted.length);
+			result = future.get();
+		} finally {
+			service.shutdown();
+		}
 
-        return encryptedIVAndText;
+		if (result == null) {
+			throw new RuntimeException("authentication result was null");
+		}
+		return result;
     }
 
-    public static String decrypt(byte[] encryptedIvTextBytes, String key) throws Exception {
-        int ivSize = 16;
-        int keySize = 16;
+    
+	private static ServiceClientCredentials createCredentials(final String clientId, final String clientSecret) throws Exception {
+		return new KeyVaultCredentials() {
 
-        // Extract IV.
-        byte[] iv = new byte[ivSize];
-        System.arraycopy(encryptedIvTextBytes, 0, iv, 0, iv.length);
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+			@Override
+			public String doAuthenticate(String authorization, String resource, String scope) {
+				try {
 
-        // Extract encrypted part.
-        int encryptedSize = encryptedIvTextBytes.length - ivSize;
-        byte[] encryptedBytes = new byte[encryptedSize];
-        System.arraycopy(encryptedIvTextBytes, ivSize, encryptedBytes, 0, encryptedSize);
+                    String token = getAccessToken(authorization, resource, 
+                        clientId, clientSecret).getAccessToken();
 
-        // Hash key.
-        byte[] keyBytes = new byte[keySize];
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(key.getBytes());
-        System.arraycopy(md.digest(), 0, keyBytes, 0, keyBytes.length);
-        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+                    System.out.println("Working? " + token);
 
-        // Decrypt.
-        Cipher cipherDecrypt = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipherDecrypt.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
-        byte[] decrypted = cipherDecrypt.doFinal(encryptedBytes);
+                    return token;
 
-        return new String(decrypted);
+				} catch (Exception ex) {
+					throw new RuntimeException(ex);
+				}
+			}
+		};
+	}
+
+    private static Properties getKafkaConfig(String sharedAccessKey, String eventHub)
+            throws IOException, FileNotFoundException {
+        Properties kafkaConfig = new Properties();
+
+        kafkaConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        kafkaConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+
+        kafkaConfig.load(new FileReader("src/main/resources/consumer.config"));
+
+        kafkaConfig.put("sasl.jaas.config",
+                ((String) kafkaConfig.get("sasl.jaas.config")).replace("${ACCESS_KEY}", sharedAccessKey));
+
+        kafkaConfig.put("bootstrap.servers",
+                ((String) kafkaConfig.get("bootstrap.servers")).replace("${EVENT_HUB}", eventHub));
+
+        kafkaConfig.put("sasl.jaas.config",
+                ((String) kafkaConfig.get("sasl.jaas.config")).replace("${EVENT_HUB}", eventHub));
+        return kafkaConfig;
     }
 }
